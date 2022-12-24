48c5d04efbac47a7da4b110c9eca0fcac8b12945
nønexist <111337838+n0nexist@users.noreply.github.com>
2022-12-24 10:34:49 +0100

    Add files via upload

---
+++ AnyXist.py
@@ -0,0 +1,65 @@
+import psutil
+import os
+import socket   
+hostname=socket.gethostname()   
+IPAddr=socket.gethostbyname(hostname)
+
+IPAddr = IPAddr.split(".")
+IPAddr = IPAddr[0]+"."+IPAddr[1]
+
+
+print("""
+   _____                ____  ___.__          __   
+  /  _  \   ____ ___.__.\   \/  /|__| _______/  |_ 
+ /  /_\  \ /    <   |  | \     / |  |/  ___/\   __\\
+/    |    \   |  \___  | /     \ |  |\___ \  |  |  
+\____|__  /___|  / ____|/___/\  \|__/____  > |__|  
+        \/     \/\/           \_/        \/        
+> python anydesk ip grabber for windows
+> made by n0nexist
+""")
+
+
+
+
+logfile = "ip-grabbati.txt"
+process_name = "anydesk"
+pidlist = list()
+hostlist = list()
+
+print("Looking for anydesk..")
+
+for proc in psutil.process_iter():
+    if process_name in proc.name().lower():
+        pidlist.append(proc.pid)
+
+if len(pidlist) == 0:
+    print("ERROR: no open anydesk found")
+    exit()
+else:
+    print("ANYDESK found: listening for connections (CNTRL-C to stop)")
+
+while True:
+    try:
+        for x in os.popen("netstat -n -a -o").read().split("\n"):
+            for y in pidlist:
+                if str(y) in x and ("SYN_SENT" in x or "ESTABILISHED" in x) and ("UDP" not in x):
+                    hostandport = x.split(":")
+                    host = hostandport[1].split(" ")[-1]
+                    port = hostandport[2].split(" ")[0]
+                    if host not in hostlist:
+                        hostlist.append(host)
+                        print("\n"+("#"*20))
+                        flag = ""
+                        if host.startswith(IPAddr):
+                            flag = "!! this may be your local ip address"
+                        print(f"""NEW CONNECTION FOUND!
+{flag}
+info:
+ip -> {host}
+port -> {port}
+
+process id -> {str(y)}""")
+    except KeyboardInterrupt:
+        print("\nCONTROL-C detected, exiting")
+        exit()
